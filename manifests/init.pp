98e76f8a25d558d5796479902d8391123f97a96a
s1rc0 <sergey.postument@gmail.com>
2014-11-19 19:15:00 +0200

    v.1.0.1b

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,41 +1,13 @@
-# == Class: el6repos
-#
-# Full description of class el6repos here.
-#
-# === Parameters
-#
-# Document parameters here.
-#
-# [*sample_parameter*]
-#   Explanation of what this parameter affects and what it defaults to.
-#   e.g. "Specify one or more upstream ntp servers as an array."
-#
-# === Variables
-#
-# Here you should define a list of variables that this module would require.
-#
-# [*sample_variable*]
-#   Explanation of how this variable affects the funtion of this class and if
-#   it has a default. e.g. "The parameter enc_ntp_servers must be set by the
-#   External Node Classifier as a comma separated list of hostnames." (Note,
-#   global variables should be avoided in favor of class parameters as
-#   of Puppet 2.6.)
-#
-# === Examples
-#
-#  class { 'el6repos':
-#    servers => [ 'pool.ntp.org', 'ntp.local.company.com' ],
-#  }
-#
-# === Authors
-#
-# Author Name <author@domain.com>
-#
-# === Copyright
-#
-# Copyright 2014 Your name here, unless otherwise noted.
-#
-class el6repos {
+class el6repos
+{
+  $repos = split($enabled_repo, ',')
 
-
+  each($repos) |$repo| {
+    file { "$repo":
+      ensure  => present,
+      path    => "/etc/yum.repos.d/${repo}.repo",
+      mode    => '0644',
+      source  => "puppet:///modules/el6repos/${repo}.repo",
+    }
+  }
 }
